63c946d2f79c9e8208074c22a07cf67a7e65acfb
bfalad22 <bolasaliu93@gmail.com>
2023-10-16 04:57:33 -0700

    0x00-MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- script that creates a table users followinh these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+	
+);
